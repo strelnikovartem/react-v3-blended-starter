c9e05c31a7c7a7298acd1048ef6c6e98b48b3242
Artem Strelnikov <piramida1237@gmail.com>
2025-08-02 11:14:33 +0200

    add_key

--- src/services/photos.ts
+++ src/services/photos.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "563492ad6f9170000100000108dc2880626e4436b3634ce1cf6b4d74";
-axios.defaults.baseURL = "https://api.pexels.com/v1/";
+const API_KEY = import.meta.env.VITE_API_KEY;
 axios.defaults.headers.common["Authorization"] = API_KEY;
 axios.defaults.params = {
   orientation: "landscape",
